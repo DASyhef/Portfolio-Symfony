5344b7cba12fe94b1d35f0a8e4506e7e9b74285b
DASyhef <daspoubelle@hotmail.fr>
2023-07-17 16:38:38 +0200

    knowledge done

---
+++ portfolio-app/app/src/Controller/KnowledgeController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Paragraf;
+use App\Repository\ParagrafRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class KnowledgeController extends AbstractController
+{
+    #[Route('/knowledge', name: 'app_knowledge')]
+    public function index(ParagrafRepository $paragrafRepository
+    ): Response
+    {
+
+        $presentations = $paragrafRepository->findAll();
+//        dd($presentations);
+
+        return $this->render('knowledge/index.html.twig', [
+            'controller_name' => 'KnowledgeController',
+            'paragrafs' => $presentations
+        ]);
+    }
+}
